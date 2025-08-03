087a7714ca30cc48ba324cec6c1443d4fc75f67c
B. Akash <akashakki2617@gmail.com>
2025-08-03 09:16:57 +0000

    make it clear it is not in perfect shape

--- src/components/icons/Pill2DoorLogo.tsx
+++ src/components/icons/Pill2DoorLogo.tsx
@@ -31,21 +31,16 @@ export function Pill2DoorLogo(props: SVGProps<SVGSVGElement>) {
       {/* Stethoscope */}
       <path
         className="pill2door-stethoscope-path"
-        d="M 45 25 C 45 38.8 35 45 25 45 C 15 45 5 35 5 25 C 5 15 15 5 25 5 C 35 5 45 11.2 45 25 Z M 190 40 A 5 5 0 0 1 190 30"
-        strokeWidth="3"
-        fill="none"
-        transform="translate(0, -2)"
-      />
-      <path
-        className="pill2door-stethoscope-path"
-        d="M 45 23 C 55 13 65 10 80 10 L 150 10 C 165 10 175 15 180 25 L 185 30 L 190 35"
+        d="M 45 23 C 55 13 65 10 80 10 L 150 10 C 165 10 175 15 180 25 L 188 33"
         strokeWidth="3"
         fill="none"
         transform="translate(0, -2)"
       />
       <circle className="pill2door-stethoscope-path" cx="190" cy="35" r="4" fill="hsl(var(--primary))" transform="translate(0, -2)"/>
-      
+       <path className="pill2door-stethoscope-path" d="M 190 39 A 5 5 0 1 1 190 31" strokeWidth="2.5" fill="none" transform="translate(0, -2)" />
+
       {/* Plus icon in circle */}
+       <circle className="pill2door-stethoscope-path" cx="25" cy="23" r="20" strokeWidth="3" fill="none"/>
       <circle className="pill2door-accent-circle" cx="25" cy="23" r="14" />
       <path
         d="M 20 23 L 30 23 M 25 18 L 25 28"
